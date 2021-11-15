28f3fc21269a244f518622fd5769a4f56a583486
Felipe Olavarria <178.felipe@gmail.com>
2021-11-15 11:42:04 -0300

    Remove Container from home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,17 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import { Container } from '@chakra-ui/react'
 import Hero from '../components/sections/hero'
 
 const Home: NextPage = () => {
   return (
-    <Container>
+    <>
       <Head>
         <title>Felipe Olavarria</title>
         <meta name="description" content="Felipe Olavarría's dev portfolio" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Hero />
-    </Container>
+    </>
   )
 }
 
