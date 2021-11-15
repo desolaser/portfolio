57d5e50a25277344dbf57e67c40afca968bc376b
Felipe Olavarria <178.felipe@gmail.com>
2021-11-15 13:03:53 -0300

    Center all text in about

--- components/sections/about.tsx
+++ components/sections/about.tsx
@@ -5,19 +5,17 @@ const About: FunctionComponent = () => {
   return (
     <Box minH="50vh" textAlign="center">
       <Heading size="xl">About Me</Heading>
-      <Box textAlign="left">
-        <Text pt="2rem">
-          I'm a FullStack Developer with three years of experience in web development.
-        </Text>
-        <Text pt=".5rem">
-          In my free time i like to learn about new web development stuff, besides
-          playing guitar and drawing.        
-        </Text>
-        <Text pt=".5rem">
-          Good team player, with six months of experience as project manager and working
-          with Kanban.
-        </Text>
-      </Box>
+      <Text pt="2rem">
+        I'm a FullStack Developer with three years of experience in web development.
+      </Text>
+      <Text pt=".5rem">
+        In my free time i like to learn about new web development stuff, besides
+        playing guitar and drawing.        
+      </Text>
+      <Text pt=".5rem">
+        Good team player, with six months of experience as project manager and working
+        with Kanban.
+      </Text>
     </Box>
   )
 }
