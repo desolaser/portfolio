501d49bce63e4f065baf0829ab50fd5ba638d8c7
Felipe Olavarria <178.felipe@gmail.com>
2021-11-15 12:24:14 -0300

    Add button and contact text to hero

--- components/sections/hero.tsx
+++ components/sections/hero.tsx
@@ -1,12 +1,23 @@
 import type { FunctionComponent } from 'react'
-import { Box, Center, Heading, Text } from '@chakra-ui/react'
+import { Box, Center, Heading, Text, Button } from '@chakra-ui/react'
 
 const Hero: FunctionComponent = () => {
   return (
     <Center h="100vh" w="full">
       <Box textAlign="center">
         <Heading size="2xl">Felipe Olavarría</Heading>
-        <Text fontSize="1.5rem">Software Engineer</Text>
+        <Text color={'purple.400'} fontSize="1.5rem" my=".5rem">
+          Software Engineer
+        </Text>
+
+        <Box my="2rem">
+          <Text fontSize={{ base: 'md', lg: 'lg' }} color={'gray.500'}>
+            We can make something great.
+          </Text>
+          <Button colorScheme="purple" my="1rem">
+            Contact me
+          </Button>
+        </Box>
       </Box>
     </Center>
   )
