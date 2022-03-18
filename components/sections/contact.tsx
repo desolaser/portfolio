53f325549e8c17b11f71a8092388004354739650
Felipe Olavarria <178.felipe@gmail.com>
2022-03-18 16:31:32 -0300

    Add useContact in contact component

--- components/sections/contact.tsx
+++ components/sections/contact.tsx
@@ -1,14 +1,26 @@
-import type { FunctionComponent } from 'react';
+import React from 'react';
 import { 
   Box, 
+  Button,
   VStack, 
   Heading, 
   Text, 
   useColorModeValue
 } from '@chakra-ui/react';
 import { FormInput } from '../forms';
+import useContact from '../../hooks/useContact';
+import Error from '../../components/elements/error';
 
-const Contact: FunctionComponent = () => {
+const Contact: React.FC = () => {
+  const { 
+    values,
+    errors,
+    loading,
+    submitError,
+    handleChange,
+    handleSubmit
+  } = useContact();
+
   const projectCardColor = useColorModeValue('gray.100', 'gray.700');
   return (
     <Box minH="50vh" mt={8} textAlign="center">
@@ -16,40 +28,49 @@ const Contact: FunctionComponent = () => {
       <Text marginY={4}>
         Send me a message if you want to build a great project.
       </Text>
-      <VStack
-        bgColor={projectCardColor}
-        shadow="md" 
-        p={8} 
-        mt={{ base: 8, md: 0 }}
-      >
-        <FormInput
-          id="name"
-          title="Name"
-          value=""
-          placeholder="Insert your name"
-          handleChange={() => {}}
-          error={{}}
-          type="text"
-        />
-        <FormInput
-          id="email"
-          title="Email"
-          value=""
-          placeholder="Insert the email"
-          handleChange={() => {}}
-          error={{}}
-          type="text"
-        />
-        <FormInput
-          id="message"
-          title="Message"
-          value=""
-          placeholder="Insert the message"
-          handleChange={() => {}}
-          error={{}}
-          type="text"
-        />
-      </VStack>
+      <form onSubmit={handleSubmit}>
+        <VStack
+          bgColor={projectCardColor}
+          shadow="md" 
+          p={8} 
+          mt={{ base: 8, md: 0 }}
+        >
+          <FormInput
+            name="name"
+            title="Name"
+            value={values.name}
+            placeholder="Insert your name"
+            handleChange={handleChange}
+            error={errors.name}
+            type="text"
+          />
+          <FormInput
+            name="email"
+            title="Email"
+            value={values.email}
+            placeholder="Insert the email"
+            handleChange={handleChange}
+            error={errors.email}
+            type="text"
+          />
+          <FormInput
+            name="message"
+            title="Message"
+            value={values.message}
+            placeholder="Insert the message"
+            handleChange={handleChange}
+            error={errors.message}
+            type="text"
+          />
+          <Button
+            colorScheme="violet"
+            loading={loading}
+          >
+            Agregar
+          </Button>
+          {submitError && <Error>{submitError}</Error>}
+        </VStack>
+      </form>
     </Box>
   );
 }
