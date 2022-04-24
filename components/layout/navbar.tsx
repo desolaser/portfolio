3c70a2522978800e25401cc42f93d36f7617d21b
Desolaser <178.felipe@gmail.com>
2022-04-24 18:26:10 -0400

    Remove navbar links to about and contact

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -236,10 +236,6 @@ interface NavItem {
 
 const NAV_ITEMS: Array<NavItem> = [
   {
-    label: 'About me',
-    href: '#about'
-  },
-  {
     label: 'Projects',
     children: [
       {
@@ -258,10 +254,6 @@ const NAV_ITEMS: Array<NavItem> = [
         href: 'https://www.imperiogamers.cl',
       },
     ],
-  },
-  {
-    label: 'Contact',
-    href: '#contact',
   },
 ];
 
