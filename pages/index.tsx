399d0d43ee2f5dd1ebd857bcfb35e4cd2d24fb0b
Desolaser <178.felipe@gmail.com>
2022-08-27 17:31:20 -0400

    Hide contact due to crawlers

--- pages/index.tsx
+++ pages/index.tsx
@@ -28,7 +28,7 @@ const Home: NextPage = () => {
       <Container maxW="container.xl" p={[10, 20]}>
         <About />
         <Projects />
-        <Contact contactRef={contactRef} />
+        {/* <Contact contactRef={contactRef} /> */}
       </Container>
     </>
   );
